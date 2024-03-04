6ecee3da16adc3d1d98861fa2b8e7deb40dc9082
aarora7 <abhay_arora@intuit.com>
2024-03-04 20:33:43 +0530

    chore: initial commit

---
+++ src/main/java/com/works/ab/streamtranslator/StreamTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.works.ab.streamtranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamTranslatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamTranslatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/works/ab/streamtranslator/StreamTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.works.ab.streamtranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamTranslatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
